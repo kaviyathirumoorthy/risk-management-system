d849cb0de586f3ca3e3e267eb85382cb69279a24
kaviyathirumoorthy <kavimoorthi2003@gmail.com>
2026-08-08 18:01:59 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/org/kaviya/riskmanagementsystem/RiskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.kaviya.riskmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskManagementSystemApplication.class, args);
+    }
+
+}
